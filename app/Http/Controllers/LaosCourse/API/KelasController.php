d06c81c588545405c0c8431e59c3b155b6b5edf3
ChristYoga123 <christianuswibisono@gmail.com>
2025-04-26 08:44:13 +0700

    feat: api course

---
+++ app/Http/Controllers/LaosCourse/API/KelasController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers\LaosCourse\API;
+
+use App\Models\Kursus;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\Helpers\ResponseFormatterController;
+
+class KelasController extends Controller
+{
+    public function index()
+    {
+        $kursus = Kursus::withCount(['mentors', 'students'])
+            ->with(['materi', 'flashSale', 'media'])->withAvg        ('reviews as avg_rating', 'kursus_murids.rating')
+            ->whereIsPublished(true)
+            ->latest()
+            ->paginate(6);
+        return ResponseFormatterController::success($kursus, 'Kursus retrieved successfully');
+    }
+
+    public function filter(Request $request)
+    {
+        $kategori = $request->has('kategori') ? explode(',', $request->kategori) : [];
+
+        $kursus = Kursus::with(['flashSale', 'materi', 'media'])->withCount(['mentors', 'students'])
+            ->whereIsPublished(true)
+            ->withAvg('reviews as avg_rating', 'kursus_murids.rating')
+            ->when(!empty($kategori), function ($query) use ($kategori) {
+                return $query->whereIn('kategori', $kategori);
+            })
+            ->latest()
+            ->paginate(6);
+
+        // Preserve query parameters in pagination links
+        $kursus->appends($request->all());
+
+        return ResponseFormatterController::success($kursus, 'Kursus retrieved successfully');
+    }
+
+    public function search(Request $request)
+    {
+        $query = $request->input('query');
+        
+        // Ensure query is at least 3 characters
+        if (!$query || strlen($query) < 3) {
+            return ResponseFormatterController::error('Query must be at least 3 characters', 400);
+        }
+        
+        // Get matching courses
+        $kursus = Kursus::with('media')->whereIsPublished(true)
+                        ->where('judul', 'like', "%{$query}%")
+                        ->orWhere('kategori', 'like', "%{$query}%")
+                        ->limit(8) // Limit to 8 results for better UX
+                        ->get()
+                        ->map(function($course) {
+                            return [
+                                'id' => $course->id,
+                                'title' => $course->judul,
+                                'slug' => $course->slug,
+                                'category' => $course->kategori->getLabel(),
+                                'thumbnail' => $course->getFirstMediaUrl('kursus-thumbnail'),
+                                'price' => $course->price ? 'Rp ' . number_format($course->harga, 0, ',', '.') : 'Free',
+                                'url' => route('course.kelas.show', $course->slug), // Change this to the actual course URL
+                            ];
+                        });
+        if ($kursus->isEmpty()) {
+            return ResponseFormatterController::error('No courses found', 404);
+        }
+        
+        return ResponseFormatterController::success($kursus, 'Courses retrieved successfully');
+    }
+
+    public function show($slug)
+    {
+        $kursus = Kursus::with(['media', 'bab.materi', 'mentors', 'techStacks.media', 'materi', 'reviews' => function ($query) {
+                $query->with('student');
+            }, 'flashSale'])
+            ->withCount(['bab', 'mentors', 'students', 'materi', 'reviews'])
+            ->withAvg('reviews as avg_rating', 'kursus_murids.rating')
+            ->whereSlug($slug)
+            ->first();
+        
+        if(!$kursus)
+        {
+            return ResponseFormatterController::error('Course not found', 404); 
+        }
+
+        return ResponseFormatterController::success($kursus, 'Course retrieved successfully');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,26 +1,35 @@
 <?php
 
-use App\Http\Controllers\LaosCourse\API\AuthController;
-use App\Service\MidtransPaymentService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Service\MidtransPaymentService;
+use App\Http\Controllers\LaosCourse\API\AuthController;
+use App\Http\Controllers\LaosCourse\API\KelasController;
 
-
-// Guest Middleware
+Route::post('payment-callback', [MidtransPaymentService::class, 'midtransCallback']);
 Route::prefix('course')->name('course.')->group(function()
 {
+    // Guest Middleware
     Route::middleware('guest.api')->group(function()
     {
-        // Midtrans Callback
-        Route::post('payment-callback', [MidtransPaymentService::class, 'midtransCallback']);
-
         // Auth Routes
         Route::prefix('auth')->name('auth.')->group(function()
         {
             Route::post('register', [AuthController::class, 'register']);
-            Route::post('login', [AuthController::class, 'login']);        });
+            Route::post('login', [AuthController::class, 'login']);        
+        });
+
+        // Kelas Routes
+        Route::prefix('kelas')->name('kelas.')->group(function()
+        {
+            Route::get('/', [KelasController::class, 'index']);
+            Route::get('/filter', [KelasController::class, 'filter']);
+            Route::get('/search', [KelasController::class, 'search']);
+            Route::get('/{slug}', [KelasController::class, 'show']);
+        });
     });
 
+    // Auth Middleware
     Route::middleware('auth.api')->group(function()
     {
         // Auth Routes
@@ -33,4 +42,3 @@ Route::prefix('course')->name('course.')->group(function()
     });
 });
 
-// Auth Middleware
